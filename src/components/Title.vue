3516068a4d62fe11cd084d7bd7413bdca8ad9f7d
小池　建 <t-koike@marsflag.com>
2023-04-25 12:16:55 +0900

    Considering introducing copilot and testing copilot

---
+++ src/components/Title.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+
+defineProps({
+  title: String,
+});
+
+</script>
+
+<template>
+  <h2>{{title}}</h2>
+</template>
+
+
+<style scoped lang="scss">
+h2 {
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+  color: #000;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,203 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import { ref } from 'vue';
+  import Title from '@/components/Title.vue';
+
+
+  const isMenuOpen = ref(false);
+
+  const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+    console.log(isMenuOpen.value);
+  };
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header>
+    <div class="logo">
+      <h1>Sneakers</h1>
+    </div>
+    <div
+      @click="toggleMenu"
+      class="hamburgerBtn"
+      :class="{ open: isMenuOpen }"
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </header>
+  <div class="kv">
+    <img src="../assets/sample.jpg" alt="">
+  </div>
+  <div class="title">
+    <Title title="New Arrivals" />
+  </div>
+  <div class="gridArea">
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+    <div class="gridAreaImg">
+      <img src="../assets/sample.jpg" alt="">
+    </div>
+  </div>
+  <div class="contactArea">
+    <form action="">
+      <input type="text" placeholder="お名前">
+      <input type="text" placeholder="メールアドレス">
+      <input type="text" placeholder="お問い合わせ内容">
+      <input type="submit" value="送信">
+    </form>
+  </div>
 </template>
+
+<style scoped lang="scss">
+
+header{
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+
+  .logo{
+
+    p{
+      font-size: 2rem;
+      font-weight: 700;
+    }
+
+  }
+
+  // .openが付いたらspanが×になる
+  .hamburgerBtn{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 2rem;
+    height: 2rem;
+    cursor: pointer;
+
+    span{
+      width: 100%;
+      height: 0.25rem;
+      background-color: #000;
+      border-radius: 10px;
+    }
+
+    &.open{
+
+      span{
+
+        &:nth-child(1){
+          transform-origin: 0% 0%;
+          transform: rotate(45deg);
+        }
+
+        &:nth-child(2){
+          opacity: 0;
+        }
+
+        &:nth-child(3){
+          transform-origin: -25% 0%;
+          transform: rotate(-45deg);
+        }
+
+      }
+
+    }
+
+  }
+
+}
+
+.kv{
+  width: 100%;
+  height: 80vh;
+  background-color: #000;
+  position: relative;
+
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.title{
+  text-align: center;
+  margin: 10px;
+}
+
+.gridArea{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 10px;
+  margin: 10px;
+
+  .gridAreaImg{
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    border-radius: 10px;
+    overflow: hidden;
+
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+
+.contactArea{
+
+  width: 60%;
+  margin: 0 auto;
+
+  form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+
+    input{
+      width: 100%;
+      height: 2rem;
+      margin: 10px;
+      padding: 0 10px;
+      border-radius: 10px;
+      border: 1px solid #000;
+    }
+
+    input[type="submit"]{
+      width: 50%;
+      height: 2rem;
+      margin: 10px;
+      padding: 0 10px;
+      border-radius: 10px;
+      border: 1px solid #000;
+      background-color: #000;
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+
+}
+
+
+</style>
